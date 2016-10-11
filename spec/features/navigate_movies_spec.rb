6425dbb9a3ae7a3d5642f7afbfcc2992dbb764c8
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-10-11 15:12:47 +0200

    refactor attributes, creat support folder

--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -2,11 +2,7 @@ require 'rails_helper'
 
 describe "Navigating movies" do
   it "allows navigation from the detail page to go to the listing page" do
-    movie1 = Movie.create(title: "Iron Man",
-                      rating: "PG-13",
-                      total_gross: 318412101.00,
-                      description: "Tony Stark builds an armored suit to fight the throes of evil",
-                      released_on: "2008-05-02")
+    movie1 = Movie.create(movie_attributes)
 
     visit movie_url(movie1)
 
@@ -16,11 +12,7 @@ describe "Navigating movies" do
   end
 
   it "allows navigation from the listing page to go the show page of one movie" do
-    movie1 = Movie.create(title: "Iron Man",
-                            rating: "PG-13",
-                            total_gross: 318412101.00,
-                            description: "Tony Stark builds an armored suit to fight the throes of evil",
-                            released_on: "2008-05-02")
+    movie1 = Movie.create(movie_attributes)
 
     visit movies_url
 
