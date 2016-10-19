9522bb689da88bce1d1bce47f3a0ba1d55544901
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-10-19 23:11:29 +0200

    model specs and refactoring

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -2,14 +2,32 @@ require 'rails_helper'
 
 describe "Viewing an individual movie" do
   it "shows the movies details" do
-    movie1 = Movie.create(movie_attributes(total_gross: 300000000.00))
+    movie1 = Movie.create(movie_attributes)
 
     visit movie_url(movie1)
 
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie1.rating)
-    expect(page).to have_text("$300,000,000.00")
     expect(page).to have_text(movie1.description)
     expect(page).to have_text(movie1.released_on)
   end
+
+  it "shows the total gross if the total gross is bigger than $50.000.000" do
+    movie = Movie.create(movie_attributes(total_gross: 60000000.00))
+
+    visit movie_url(movie)
+
+    expect(page).to have_text("$60,000,000.00")
+  end
+
+
+  it "shows 'Flop!' if the total gross is less than $50.000.000" do
+    movie = Movie.create(movie_attributes(total_gross: 30000000.00))
+
+    visit movie_url(movie)
+
+    expect(page).to have_text("Flop!")
+  end
+
+
 end
