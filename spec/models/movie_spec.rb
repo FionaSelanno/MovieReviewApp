4af3a02ae787b83f159879e6c08a05bf0573181f
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-10-20 15:15:36 +0200

    edit-feature with specs

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe "A movie" do
-  it "is a flop if the total_gross is less then 50000000.00" do
+  it "is a flop if the total_gross is less then $50000000.00" do
     movie = Movie.new(total_gross: "40000000.00")
 
     expect(movie.flop?).to eq(true)
   end
 
-  it "is not a flop if the total_gross is bigger then 50000000.00" do
+  it "is not a flop if the total_gross is bigger then $50000000.00" do
     movie = Movie.new(total_gross: "60000000.00")
 
     expect(movie.flop?).to eq(false)
