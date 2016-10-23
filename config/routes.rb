730d94df0601060ab030ef864fb25d57fcd66fc6
Fiona Selanno <Fiona@MacBook-Pro-van-Fiona.local>
2016-10-23 16:46:13 +0200

    Refactoring form and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   root "movies#index"
 
-  # resources :movies
-
-  post "/movies"      => "movies#create"
-  get "movies/new"    => "movies#new", as: "new_movie"
-  get "/movies/:id"   => "movies#show", as: "movie"
-  get "/movies/:id/edit"  => "movies#edit", as: "edit_movie"
-  patch "movies/:id"  => "movies#update"
-  get "/movies"       => "movies#index"
-
-
+  resources :movies
+  # get "/movies"       => "movies#index"
+  # post "/movies"      => "movies#create"
+  # get "movies/new"    => "movies#new", as: "new_movie"
+  # get "/movies/:id/edit"  => "movies#edit", as: "edit_movie"
+  # get "/movies/:id"   => "movies#show", as: "movie"
+  # patch "/movies/:id" => "movies#update"
+  # put "/movies/:id"   => "movies#update"
+  
 end
